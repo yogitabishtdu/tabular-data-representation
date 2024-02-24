035edb3604db3f90c8b9473428cecc01da88e3bd
Yogita <yogitabishtdu@gmail.com>
2024-02-24 17:45:31 +0100

    folder structure change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import DataContextProvider from "./components/DataContext";
+import DataContextProvider from "../src/contexts/DataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
